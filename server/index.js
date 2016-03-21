cfcd3ffd2b535dec9da911b80665dc5a36d76282
Chyld Medford <chyld.medford@gmail.com>
2016-03-21 18:53:58 +0000

    added more tests

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
